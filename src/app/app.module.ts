8ecec56d66e1585c58d173d4123422954e98a153
abanoub maged <abanoubmagedmaher@gmail.com>
2024-07-22 21:03:34 +0300

    "Removed ProductsComponent from AppComponent, moved NavBarComponent to CoreModule, and added ShopComponent to ShopModule."

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 import { HttpClientModule} from '@angular/common/http';
 import { FooterComponent } from './Componnets/footer/footer.component';
 import { RegisterComponent } from './Componnets/register/register.component';
 import { HomeComponent } from './Componnets/home/home.component';
 import { ProductsComponent } from './Componnets/products/products.component';
+import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
     FooterComponent,
-  
     RegisterComponent,
     HomeComponent,
     ProductsComponent
@@ -22,7 +20,8 @@ import { ProductsComponent } from './Componnets/products/products.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
